25482ace352bcc68fd24cb9680707468bdbecfb8
Jose Alves <jose.sousalves@gmail.com>
2023-02-11 21:04:30 +0000

    chore(prettier): prettier update on Stack.styles file

--- packages/ui/components/Stack/Stack.styles.ts
+++ packages/ui/components/Stack/Stack.styles.ts
@@ -1,8 +1,8 @@
 import styled, { css } from 'styled-components';
 import { gridGap, width } from 'styled-system';
 
-import Box from '../Box';
 import { StackProps } from './Stack.types';
+import Box from '../Box';
 
 export const Stack = styled(Box)<StackProps>`
   flex-direction: ${(props) => (props.vertical ? 'column' : 'row')};
