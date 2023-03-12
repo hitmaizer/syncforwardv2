6b5e46652f8a54e4d8156217971c9853c59357a9
Jose Alves <jose.sousalves@gmail.com>
2023-03-12 11:41:07 +0000

    chore(graphql): remove all graphql related packages/files

--- apps/web/src/providers/app.tsx
+++ apps/web/src/providers/app.tsx
@@ -2,19 +2,15 @@
 
 import { ReactNode } from 'react';
 
-import { ApolloProvider } from '@apollo/client';
-import client from '@lib/apollo';
 import { BreakpointProvider } from '@ui-hooks/useBreakpoint';
 import { SingleThemeProvider } from '@ui-hooks/useSingleTheme';
 import dark from '@styles/theme/dark/dark.theme';
 
 const AppProvider = ({ children }: { children: ReactNode }) => {
   return (
-    <ApolloProvider client={client}>
-      <SingleThemeProvider theme={dark}>
-        <BreakpointProvider>{children}</BreakpointProvider>
-      </SingleThemeProvider>
-    </ApolloProvider>
+    <SingleThemeProvider theme={dark}>
+      <BreakpointProvider>{children}</BreakpointProvider>
+    </SingleThemeProvider>
   );
 };
 
