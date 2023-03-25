12a53cd56a3df3904c40874d397fece7c27b2160
Jose Alves <jose.sousalves@gmail.com>
2023-03-25 19:09:01 +0000

    chore(web/consts): add navItems const file

---
+++ apps/web/src/constants/navItems.ts
@@ -0,0 +1,6 @@
+export const navItems = [
+  { label: 'Home', href: '/' },
+  { label: 'Releases', href: '/releases' },
+  { label: 'Podcasts', href: '/podcasts' },
+  { label: 'Store', href: '/store' },
+];
