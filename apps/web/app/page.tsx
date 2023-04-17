2d916174db246d025b1f50a2cc9271930baf36e7
Jose Alves <jose.sousalves@gmail.com>
2023-04-17 19:35:40 +0100

    chore(web): update Home to render HeroSection

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,30 +1,10 @@
-import Image from 'next/image';
-
-async function getPosts() {
-  const res = await fetch('http://localhost:1337/api/posts?populate=*');
-  const posts = await res.json();
-  return posts?.data;
-}
+import HeroSection from '@components/HeroSection';
 
 export default async function HomePage() {
-  const posts = await getPosts();
   return (
     <div>
-      {posts.map((post) => {
-        return (
-          <div key={post.id}>
-            <h1>{post.attributes.title}</h1>
-            <Image
-              src={post.attributes.image.data.attributes.url}
-              width={post.attributes.image.data.attributes.width}
-              height={post.attributes.image.data.attributes.height}
-              alt={post.attributes.title}
-              placeholder="blur"
-              blurDataURL={post.attributes.image.data.attributes.placeholder}
-            />
-          </div>
-        );
-      })}
+      {/* @ts-expect-error Server Component */}
+      <HeroSection />
     </div>
   );
 }
